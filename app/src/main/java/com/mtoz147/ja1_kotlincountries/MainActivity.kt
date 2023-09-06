9f2f7416087b4a771ee4dfe3f10793130fb68272
Mahmut OZYURT <mtoz147@gmail.com>
2023-09-06 06:41:49 +0300

    App Design Started

---
+++ app/src/main/java/com/mtoz147/ja1_kotlincountries/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mtoz147.ja1_kotlincountries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
