c64d03bf9ee64ac372453d49cfa06c5e90203ad4
Mahmut OZYURT <mtoz147@gmail.com>
2023-09-10 19:04:14 +0300

    MVVM Structure has been started to implement.  -Model -> Country data class has been created.   -Primary constructors has been added to Model.

---
+++ app/src/main/java/com/mtoz147/ja1_kotlincountries/model/Model.kt
@@ -0,0 +1,10 @@
+package com.mtoz147.ja1_kotlincountries.model
+
+data class Country(
+    val countryName: String?,
+    val countryRegion: String?,
+    val countryCapital: String?,
+    val countryCurrency: String?,
+    val countryLanguages: String?,
+    val imageUrl: String?,
+)
